efb5ac1e854f20ef9bf69b8eef15dd781a22722c
crystal <noelani.villanueva@gmail.com>
2021-08-27 16:59:07 -0700

    react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
+import Homepage from "./components/homepage/index";
+import Login from "./components/login/index";
+import Signup from "./components/signup/index";
+
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route
+          exact
+          path="/login"
+          render={() => {
+            return <Login />;
+          }}
+        />
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return <Homepage />;
+          }}
+        />
+
+        <Route
+          exact
+          path="/signup"
+          render={() => {
+            return <Signup />;
+          }}
+        />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/signup/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function index() {
+  return <div>Signup</div>;
+}
+
+export default index;
