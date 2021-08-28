378cb568521d6e9e324133bbb5992d5dfd68009c
crystal <noelani.villanueva@gmail.com>
2021-08-27 19:47:27 -0700

    homepage additions

--- frontend/src/components/homepage/index.js
+++ frontend/src/components/homepage/index.js
@@ -1,7 +1,40 @@
 import React from "react";
+import "./homepage.css";
 
-function index() {
-  return <div>homepage</div>;
+function Index() {
+  const handleSearch = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      homepage
+      <div className="homeParent">
+        <div className="query">
+          <h2>query</h2>
+          <div>filter</div>
+          <div>categories</div>
+          <div>price range</div>
+        </div>
+        <div className="mainContent">
+          main content
+          <div className="search">
+            search bar
+            <input
+              onChange={(e) => {
+                handleSearch(e);
+              }}
+            />
+          </div>
+          <div className="sort">
+            sort
+            <span>New</span>
+            <span>Price</span>
+          </div>
+          <div>body</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default index;
+export default Index;
