03766434501089b2ad2997cc1bff35898fd7cefb
crystal <noelani.villanueva@gmail.com>
2021-08-27 18:38:38 -0700

    handle sign up fetch initiated and handling the auth response to push the user to the /dashboard page

--- frontend/src/components/signup/index.js
+++ frontend/src/components/signup/index.js
@@ -19,6 +19,25 @@ function Index() {
         alert("something");
     }
   };
+  const handleSignUp = (e, user, password) => {
+    e.preventDefault();
+    // fetch(api + "/signup", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ user: user, password: password }),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => this.handleAuthResponse(data))
+    //   .catch(console.log);
+  };
+
+  const handleAuthResponse = (data) => {
+    //confirm that the data is data.user, which is true, then
+    //push the user to the /dashboard route
+  };
+
   return (
     <div>
       Signup
@@ -53,7 +72,9 @@ function Index() {
             placeholder="re-enter password"
           />
         </div>
-        <button>Sign Up</button>
+        <button onClick={(e) => handleSignUp(e, user, password)}>
+          Sign Up
+        </button>
       </form>
       <div>
         Already have an account?
