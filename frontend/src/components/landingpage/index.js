dfe190602e134481aa411736b3046d0dc9ba45ba
crystal <noelani.villanueva@gmail.com>
2021-08-27 23:57:41 -0700

    landing page

--- frontend/src/components/landingpage/index.js
+++ frontend/src/components/landingpage/index.js
@@ -4,13 +4,14 @@ function index() {
   return (
     <div>
       Hi! Welcome to {}!
-      <div>
-        Learn more
-        <Link to="/learn">Learn More</Link>
-      </div>
-      <div>
-        Login/Sign Up
-        <Link to="/login">Login/Signup</Link>
+      <div className="space-x-1.5">
+        <button className="border border-solid bg-red-400 rounded-xl p-1">
+          <Link to="/learn">Learn More</Link>
+        </button>
+
+        <button className="border border-solid bg-blue-400 rounded-xl p-1">
+          <Link to="/login">Login/Signup</Link>
+        </button>
       </div>
     </div>
   );
